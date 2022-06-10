4ac8c7d0b571823ac2d0a7b828495b0ea42535f2
igorek19882020 <igorek20081988@mail.ru>
2022-06-10 21:01:38 +0300

    adaptive all

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,14 @@ $(function () {
       ' <img class="slider__arrow slider__arrow-left" src="../images/arrow-left.svg" alt="arrow-left">',
     nextArrow:
       ' <img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="arrow-right">',
+      responsive: [
+        {
+          breakpoint:480,
+          settings:{
+            arrows: false
+          }
+        }
+      ]
   });
 
   $(".menu__btn").on("click", function () {
