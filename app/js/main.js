4ece9e201434d7ae6d6f22890182bdcfdb4883f3
igorek19882020 <igorek20081988@mail.ru>
2022-06-10 15:44:35 +0300

    adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
-$(function(){
-    $('.slider__box').slick({
-        prevArrow:' <img class="slider__arrow slider__arrow-left" src="../images/arrow-left.svg" alt="arrow-left">',
-        nextArrow:' <img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="arrow-right">'
-    });
-     
-})
+$(function () {
+  $(".slider__box").slick({
+    prevArrow:
+      ' <img class="slider__arrow slider__arrow-left" src="../images/arrow-left.svg" alt="arrow-left">',
+    nextArrow:
+      ' <img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="arrow-right">',
+  });
+
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleClass("active");
+  });
+});
